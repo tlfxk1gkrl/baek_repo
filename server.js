82223f7f51ee0580dcfb0eb4927f61085ed3d793
tlfxk1gkrl <baek.sm9059@gmail.com>
2023-02-17 22:27:55 +0900

    change ws to socket.io and make chat ui(HTML)

--- server.js
+++ server.js
@@ -1,21 +1,49 @@
 const express = require("express");
 const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-app.use("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(8080);
+io.on("connection", (socket) => {
+  //connect
+  console.log("new user connected");
 
-const WebSocket = require("ws");
+  //disconnect
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 
-const socket = new WebSocket.Server({
-  port: 8081,
-});
-
-socket.on("connection", (ws, req) => {
-  ws.on("message", (msg) => {
-    console.log("user : " + msg);
-    ws.send("bye");
+  //get message
+  socket.on("chat message", (msg) => {
+    console.log("message : " + msg);
+    io.emit("chat message", msg);
   });
 });
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
+
+// app.use("/", function (req, res) {
+//   res.sendFile(__dirname + "/index.html");
+// });
+
+// app.listen(8080);
+
+// const WebSocket = require("ws");
+
+// const socket = new WebSocket.Server({
+//   port: 8081,
+// });
+
+// socket.on("connection", (ws, req) => {
+//   ws.on("message", (msg) => {
+//     console.log("user : " + msg);
+//     ws.send("return");
+//   });
+// });
