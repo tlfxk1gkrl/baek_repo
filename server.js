6f06f00a2404b76a2bb98579e7a9d8224a4a0cca
tlfxk1gkrl <baek.sm9059@gmail.com>
2023-03-02 23:18:00 +0900

    add transrate API from naver papago

--- server.js
+++ server.js
@@ -4,17 +4,98 @@ const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const bodyParser = require("body-parser");
+
+//api-key
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+const NAVER_CLIENT_ID = process.env.process.env.NAVER_CLIENT_ID;
+const NAVER_CLIENT_KEY = process.env.process.env.NAVER_CLIENT_KEY;
 
 //openAI
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
-  apiKey: "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",
+  apiKey: OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//papago
+app.post("/detectLangs", function (req, res) {
+  var api_url = "https://openapi.naver.com/v1/papago/detectLangs";
+  var request = require("request");
+  var query = req.body.data;
+  var options = {
+    url: api_url,
+    form: { query: query },
+    headers: {
+      "X-Naver-Client-Id": NAVER_CLIENT_ID,
+      "X-Naver-Client-Secret": NAVER_CLIENT_KEY,
+    },
+  };
+  request.post(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.writeHead(200, { "Content-Type": "text/json;charset=utf-8" });
+      res.end(body);
+    } else {
+      res.status(response.statusCode).end();
+      console.log("error = " + response.statusCode);
+    }
+  });
+});
+
+app.post("/translate", function (req, res) {
+  var api_url = "https://openapi.naver.com/v1/papago/n2mt";
+  var request = require("request");
+  var query = req.body.msg;
+  var lan = req.body.lan;
+  var options = {
+    url: api_url,
+    form: { source: lan, target: "en", text: query },
+    headers: {
+      "X-Naver-Client-Id": NAVER_CLIENT_ID,
+      "X-Naver-Client-Secret": NAVER_CLIENT_KEY,
+    },
+  };
+  request.post(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.writeHead(200, { "Content-Type": "text/json;charset=utf-8" });
+      res.end(body);
+    } else {
+      res.status(response.statusCode).end();
+      console.log("error = " + response.statusCode);
+    }
+  });
+});
+
+app.post("/returntranslate", function (req, res) {
+  var api_url = "https://openapi.naver.com/v1/papago/n2mt";
+  var request = require("request");
+  var query = req.body.msg;
+  var lan = req.body.lan;
+  var options = {
+    url: api_url,
+    form: { source: "en", target: lan, text: query },
+    headers: {
+      "X-Naver-Client-Id": NAVER_CLIENT_ID,
+      "X-Naver-Client-Secret": NAVER_CLIENT_KEY,
+    },
+  };
+  request.post(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.writeHead(200, { "Content-Type": "text/json;charset=utf-8" });
+      res.end(body);
+    } else {
+      res.status(response.statusCode).end();
+      console.log("error = " + response.statusCode);
+    }
+  });
 });
 
 io.on("connection", (socket) => {
