1a60905646b4f00e7bd30f5a662aacb6b9875949
tlfxk1gkrl <baek.sm9059@gmail.com>
2023-04-17 22:56:20 +0900

    add jwt test(login)

--- src/script/login.ts
+++ src/script/login.ts
@@ -1,4 +1,8 @@
+import { Jwt } from "jsonwebtoken";
+
 const form = document.getElementById("login-form") as HTMLFormElement;
+
+let token;
 
 form.addEventListener("submit", async (event: Event) => {
   event.preventDefault(); // prevent the default form submission
@@ -8,20 +12,33 @@ form.addEventListener("submit", async (event: Event) => {
   const password = (document.getElementById("password") as HTMLInputElement)
     .value;
 
-  console.log(`Username: ${username}, Password: ${password}`);
-
   // send the login request to the server and wait for the response
-  const response = await fetch("/login", {
+  const response = await fetch("http://localhost:8080/tokenCreate", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ username, password }),
+    body: JSON.stringify({ username: username, name: "", password: password }),
   });
 
   if (response.ok) {
     // authentication was successful, redirect to the home page
-    window.location.href = "/chat";
+    const body = await response.json();
+    token = body.data.token;
+    console.log(token);
+    fetch("http://localhost:8080/checkToken", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.code == "200") {
+          window.location.href = "/chat";
+        }
+      })
+      .catch((err) => console.log(err));
   } else {
     // authentication failed, display an error message
     const error = await response.text();
